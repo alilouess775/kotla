701582e343cf1712e958f99b9aef5de83abac9d1
alilouess775 <110328114+alilouess775@users.noreply.github.com>
2022-08-02 01:28:29 +0100

    Add files via upload

---
+++ controleur.php
@@ -0,0 +1,13 @@
+ <?php
+
+   if(isset($_POST["login"])){
+    echo $_POST["username"];
+   }
+
+ 
+ 
+ 
+ 
+ 
+ 
+ ?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kotla</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+   <form action="controleur.php" method = "POST">
+
+   <input type="text" name="username" placeholder="Username"><br>
+   <input type="password" name="passowrd" placeholder="Password"><br>
+   <button type="submit" name="login">Login</button>
+
+   </form>
+
+
+</body>
+</html>
